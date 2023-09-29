fbc9d8d4c494cb10e03988880ce52b2b04082f17
andro <97570105+4ndr0666@users.noreply.github.com>
2023-09-29 17:51:29 -0500

    deleted vscode and updated main

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 function fetchMedia() {
-    const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
-    const urlInput = document.getElementById("urlInput").value;
-    const TARGET_URL = urlInput || "https://redditpx.jeffjose.cloud/r/lakebell";
-
-    fetch(PROXY_URL + TARGET_URL, {
+    const PROXY_URL = "https://cors.io/?";  // New Proxy URL
+    const TARGET_URL = "https://redditpx.jeffjose.cloud/r/lakebell";
+    const inputField = document.getElementById('userInput');
+    const userURL = inputField ? inputField.value : TARGET_URL;
+    
+    fetch(PROXY_URL + userURL, {
         method: 'GET',
         headers: {
             'X-Requested-With': 'XMLHttpRequest'
@@ -11,9 +12,10 @@ function fetchMedia() {
     })
     .then(response => {
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            console.error(`HTTP Status: ${response.status}, Status Text: ${response.statusText}`);
+            throw new Error('Network response was not ok. This could be a CORS issue.');
         }
-        return response.blob();  // Convert the response data to a blob
+        return response.blob();
     })
     .then(imageBlob => {
         const imageUrl = URL.createObjectURL(imageBlob);
@@ -21,12 +23,11 @@ function fetchMedia() {
     })
     .catch(error => {
         console.error('There was an error!', error);
-        alert(`Error: ${error.message}`);
     });
 }
 
 function displayImage(url) {
     const img = document.createElement('img');
     img.src = url;
-    document.getElementById("mediaDiv").appendChild(img);
+    document.body.appendChild(img);
 }
